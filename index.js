815569fef05b2b462842bb9c095e17f175aa43b7
priyakhakrode02 <piakhakrode@gmail.com>
2025-05-01 00:13:11 +0530

    delete module created

--- index.js
+++ index.js
@@ -23,6 +23,28 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/chatapp');
 }
 
+//actual chat delete.
+
+app.delete("/chats/:id", async (req, res) => {
+    console.log("Received DELETE request for:", req.params.id);
+    
+    let { id } = req.params;
+    let deleteChat = await Chat.findByIdAndDelete(id);
+    console.log(deleteChat)
+    res.redirect("/chats");
+
+});
+
+
+//delete page route  (when user click's delete button, user will be redirected to this route)
+
+app.get("/chats/:id/delete", async (req, res) => {
+
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+
+    res.render("delete.ejs", { chat });
+});
 
 // actual update route
 
@@ -31,11 +53,12 @@ app.put("/chats/:id", async (req, res) => {
     let {id} = req.params;
     let {newMessage: edited_msg } = req.body; //Destructuring assignment happens from right to left. It extracts newMessage from req.body and assigns it to edited_msg.(like this: edited_msg = req.body.newMessage;)
     console.log(edited_msg);
-    let updatedChat = await Chat.findByIdAndUpdate(id, {msg: edited_msg}, {new: true});//({new: true}: Return the updated(new) document instead of the old one.)
+    let updatedChat = await Chat.findByIdAndUpdate(id, {msg: edited_msg}, {new: true}); //({new: true}: Return the updated(new) document instead of the old one.)
     console.log(updatedChat);
     res.redirect("/chats");
 
 });
+
 
 //edit page route
 app.get("/chats/:id/edit", async (req, res) => {
